b0ca7505735acd7b97df017c6891a0a712222191
Paphawarintr Tienpasertkij <64551424+mtienpasertkij@users.noreply.github.com>
2023-02-09 21:04:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,18 +42,18 @@ try:
 
 ###################
 
-streamlit.stop()
+## streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+##my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+##my_cur = my_cnx.cursor()
+##my_cur.execute("SELECT * FROM fruit_load_list")
+##my_data_rows = my_cur.fetchall()
+##streamlit.header("The fruit load list contains:")
+##streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+##add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+##streamlit.write('Thanks for adding ', add_my_fruit)
 
-my_cur.execute("INSERT INTO fruit_load_list values ('from streamlit')")
+##my_cur.execute("INSERT INTO fruit_load_list values ('from streamlit')")
 
 
